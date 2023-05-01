4e9d7a5c88d6290dcd8405c5ec9a2f9127994796
Alexandr Tatarinov <tatarinov.work@bk.ru>
2023-05-01 06:34:45 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N от 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string s = "";
+
+while (n < 1)
+{
+    Console.Write("Вы ошиблись!\nВведите число N от 1: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0) 
+        s = s + ($"{i } ");
+}
+
+Console.WriteLine($"{n} -> {s} ");
